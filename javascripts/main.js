dcbe48d4c7a014e17a6094084160ff98d0a2c0cb
Latasha Simmons <lsimmons1832@gmail.com>
2017-05-15 19:35:48 -0500

    loading info from firebase

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,27 @@
-console.log("I'm linked");
+app.run((FIREBASE_CONFIG) =>{
+	firebase.initializeApp(FIREBASE_CONFIG);
+});
+
+app.controller("ItemCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
+	$scope.contacts = [];
+
+		let getContacts = () =>{  
+		let list = []; 
+		return $q((resolve, reject) =>{
+			$http.get(`${FIREBASE_CONFIG.databaseURL}/contacts.json`)
+			.then((fbList)=>{
+          var addressCollection = fbList.data;
+          Object.keys(addressCollection).forEach((key) => {
+            addressCollection[key].id=key;
+            list.push(addressCollection[key]);
+            console.log("my list", list);
+          });
+          resolve(list);
+			}).catch((error) =>{
+				reject(error);
+			});
+		});
+	};
+
+getContacts();
+});
